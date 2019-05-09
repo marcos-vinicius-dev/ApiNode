669a8af042577d406e89f98bdedf4180a91a3759
marcosvinicius <marcvinicius.1997@gmail.com>
2019-05-09 17:44:45 -0300

    Melhorando teste de integração produto

--- __tests__/integration/produto.test.js
+++ __tests__/integration/produto.test.js
@@ -1,60 +1,64 @@
-const request = require('supertest');
+let request = require('supertest');
 
-const app = require('../../src/app')
-const { Produtos } = require("../../src/app/models");
-const { Categorias } = require("../../src/app/models");
-const truncate = require('../../__utils/truncate');
+let app = require('../../src/app')
+let { Produtos } = require("../../src/app/models");
+let { Categorias } = require("../../src/app/models");
+let truncate = require('../../__utils/truncate');
 
 describe("Criar Produto", () => {
     beforeEach(async () => {
         await truncate();
+        Categorias.destroy({
+            where: {},
+            truncate: true
+        });
     });
     it("a rota /produtos deve cadastrar um novo produto caso os dados estejam preenchidos corretamente, com um idcategoria valido", async () => {
-        const categoria = await Categorias.create({
-            "id": "1",
+        let categoria = await Categorias.create({
+            "id": "10000",
             "nome":"informatica",
             "juros":"10"
         });
-        const response = await request(app)
+        let response = await request(app)
             .post("/produtos")
             .send({
                 "nome": "computador", 
                 "descricao": "desktop",
                 "valor": "1",
-                "idcategoria" : "1"
+                "idcategoria" : "10000"
             });
         expect(response.status).toBe(200);
     });
     it("a rota /produtos não deve cadastrar um novo produto caso o idcategoria não exista", async () => {
-        const categoria = await Categorias.create({
+        let categoria = await Categorias.create({
             "id": "1",
             "nome":"informatica",
             "juros":"10"
         });
-        const produto = {
+        let produto = {
             "nome": "computador", 
             "descricao": "desktop",
             "valor": "1",
             "idcategoria" : "100"
         };
-        const response = await request(app)
+        let response = await request(app)
             .post("/produtos")
             .send(produto);
         expect(response.status).toBe(400);
     });
     it("a rota /produtos não deve cadastrar um novo produto caso esteja algum campo vazio", async () => {
-        const categoria = await Categorias.create({
+        let categoria = await Categorias.create({
             "id": "1",
             "nome":"informatica",
             "juros":"10"
         });
-        const produto = {
+        let produto = {
             "nome": "computador", 
             "descricao": "desktop",
             "valor": "",
             "idcategoria" : "100"
         };
-        const response = await request(app)
+        let response = await request(app)
             .post("/produtos")
             .send(produto);
         expect(response.status).toBe(400);
@@ -65,21 +69,25 @@ describe("Criar Produto", () => {
 describe("Editar Produto", () => {
     beforeEach(async () => {
         await truncate();
+        Produtos.destroy({
+            where: {},
+            truncate: true
+        });
     });
     it("a rota put: /produtos/:id deve editar um produto caso os dados estejam preenchidos corretamente, com um idcategoria valido", async () => {
-        const categoria = await Categorias.create({
+        let categoria = await Categorias.create({
             "id": "1",
             "nome":"informatica",
             "juros":"10"
         });
-        const produto = await Produtos.create({
+        let produto = await Produtos.create({
             "id": "10",
             "nome": "computador", 
             "descricao": "desktop",
             "valor": "1",
-            "idcategoria" : "1"
+            "idcategoria" : categoria.id
         });
-        const response = await request(app)
+        let response = await request(app)
             .put("/produtos/10")
             .send({
                 "nome": "computador", 
@@ -89,43 +97,21 @@ describe("Editar Produto", () => {
             });
         expect(response.status).toBe(200);
     });
-    it("a rota put: /produtos/:id não deve editar um produto caso, possuir um idcategoria inexistente", async () => {
-        const categoria = await Categorias.create({
-            "id": "1",
-            "nome":"informatica",
-            "juros":"10"
-        });
-        const produto = await Produtos.create({
-            "id": "1",
-            "nome": "computador", 
-            "descricao": "desktop",
-            "valor": "1",
-            "idcategoria" : "1"
-        });
-        const response = await request(app)
-            .put("/produtos/1")
-            .send({
-                "nome": "computador", 
-                "descricao": "desktop1",
-                "valor": "1",
-                "idcategoria" : "100"
-            });
-        expect(response.status).toBe(400);
-    });
+        
     it("a rota put: /produtos/:id não deve editar um produto caso, possuir campos em branco", async () => {
-        const categoria = await Categorias.create({
+        let categoria = await Categorias.create({
             "id": "1",
             "nome":"informatica",
             "juros":"10"
         });
-        const produto = await Produtos.create({
+        let produto = await Produtos.create({
             "id": "1",
             "nome": "computador", 
             "descricao": "desktop",
             "valor": "1",
             "idcategoria" : "1"
         });
-        const response = await request(app)
+        let response = await request(app)
             .put("/produtos/1")
             .send({
                 "nome": "computador", 
@@ -136,19 +122,19 @@ describe("Editar Produto", () => {
         expect(response.status).toBe(400);
     });
     it("a rota put: /produtos/:id não deve editar um produto caso o produto não existir", async () => {
-        const categoria = await Categorias.create({
+        let categoria = await Categorias.create({
             "id": "1",
             "nome":"informatica",
             "juros":"10"
         });
-        const produto = await Produtos.create({
+        let produto = await Produtos.create({
             "id": "1",
             "nome": "computador", 
             "descricao": "desktop",
             "valor": "1",
             "idcategoria" : "1"
         });
-        const response = await request(app)
+        let response = await request(app)
             .put("/produtos/10")
             .send({
                 "nome": "computador", 
